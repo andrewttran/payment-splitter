c0c0b9ab1e26d1d12d0498293646042ab37ce30b
Andrew Tran <attran2020@gmail.com>
2021-09-22 13:32:02 -0500

    feat: homepage loading

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Typography } from '@material-ui/core';
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import LoginButton from '../components/auth/LoginButton';
+import Loading from './Loading';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isAuthenticated) {
+    return <Redirect push to="/groups" />;
+  }
+
+  return (
+    <div>
+      <Typography>Home</Typography>
+      <LoginButton />
+    </div>
+  );
 };
 
 export default Home;
